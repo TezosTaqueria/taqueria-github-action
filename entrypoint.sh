c7071ff3917cc78afa0177d80e7c5b781bb5d32c
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-11 16:32:31 -0700

    chore: added entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,14 @@
-#!/bin/sh -l
+#!/bin/bash
 
 # if $2 is equal to "compile", then run the following
 # if [ "$2" = "bash" ]; then
 #     /bin/bash
-# ls -la
-# if [ "$2" == "init" ]; then
-#     ls -la
-#     taq -p $1 $2
-#     cd $1
-#     npm init -y
-# else
-#     taq -p $1 $2
-# fi
-
-
-exit 1
+ls -la
+if [ "$2" == "init" ]; then
+    ls -la
+    taq -p $1 $2
+    cd $1
+    npm init -y
+else
+    taq -p $1 $2
+fi
