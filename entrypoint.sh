e8f88b9ab23bc52c0d9087c32ab885ba5c1684e4
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-11 10:22:18 -0700

    chore: remove bash condition in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # if $2 is equal to "compile", then run the following
-if [ "$2" = "bash" ]; then
-    /bin/bash
-elif [ "$2" = "init" ]; then
+# if [ "$2" = "bash" ]; then
+#     /bin/bash
+if [ "$2" = "init" ]; then
     taq -p $1 $2
     cd $1
     npm init -y
