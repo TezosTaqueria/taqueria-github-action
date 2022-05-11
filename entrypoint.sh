cb61d79b5677022b9a4dafeec98f5356e7b1fbc9
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-11 16:15:14 -0700

    chore: test broken entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/bin/sh -l
 
 # if $2 is equal to "compile", then run the following
 # if [ "$2" = "bash" ]; then
 #     /bin/bash
-ls -la
-if [ "$2" == "init" ]; then
-    ls -la
-    taq -p $1 $2
-    cd $1
-    npm init -y
-else
-    taq -p $1 $2
-fi
+# ls -la
+# if [ "$2" == "init" ]; then
+#     ls -la
+#     taq -p $1 $2
+#     cd $1
+#     npm init -y
+# else
+#     taq -p $1 $2
+# fi
+
+
+exit 1
