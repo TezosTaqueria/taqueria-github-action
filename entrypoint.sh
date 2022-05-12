25b8604666164d5d49a17b95959480edfd7db98f
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-11 17:44:59 -0700

    chore: try list plugin requests

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # if $2 is equal to "compile", then run the following
 # if [ "$2" = "bash" ]; then
 #     /bin/bash
-export PROJECT_DIR=/home/runner/work/taqueria-github-action/taqueria-github-action/new-plugin-project
+export PROJECT_DIR=/home/runner/work/taqueria-github-action/taqueria-github-action
 if [ "$2" == "init" ]; then
     ls -la
     taq -p $1 $2
@@ -13,6 +13,8 @@ elif [ "$2" == "compile" ]; then
     taq -p $1 compile --logPluginRequests
 else
     ls -la
+    ls -ltr /home/runner/work/taqueria-github-action/taqueria-github-action
+    echo "The value of -p is $1"
     echo $PROJECT_DIR
     taq -p $1 $2
 fi
