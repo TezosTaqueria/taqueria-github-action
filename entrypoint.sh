66c37a30f20250aa9cd7f1f65573391479223ab6
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-11 17:06:40 -0700

    chore: try hardcoded project directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # if $2 is equal to "compile", then run the following
 # if [ "$2" = "bash" ]; then
 #     /bin/bash
+export PROJECT_DIR=/home/runner/work/taqueria-github-action/taqueria-github-action/new-plugin-project
 if [ "$2" == "init" ]; then
     ls -la
     taq -p $1 $2
