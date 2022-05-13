7c67d45b9fba4431c1be7917df8380ed7600a412
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 22:42:39 -0700

    chore: debug issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-echo "$2"
-echo "$TASK_NAME"
-# if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$TASK_NAME" ];then
-#     echo "No project name or task name provided"
-#     exit 1
-# fi
+echo "$INPUT_TASK"
+if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ];then
+    echo "No project name or task name provided"
+    exit 1
+fi
 
-
-# PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
-# # When the taq command is init
-# if [ "$TASK_NAME" == "init" ]; then
-#     taq -p "$INPUT_PROJECT_NAME" "$TASK_NAME"
-#     cd "$INPUT_PROJECT_NAME" || exit 1
-#     ls -la
-#     npm init -y
-# else
-#     echo "$PROJECT_DIR"
-#     taq -p "$INPUT_PROJECT_NAME" "$2"
-# fi
+if [ -z "$INPUT_PROJECT_NAME" ];then
+    PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
+    if [ "$INPUT_TASK" == "init" ]; then
+        taq "$INPUT_TASK"
+        npm init -y
+    else
+        echo "$PROJECT_DIR"
+        taq "$INPUT_TASK"
+    fi
+else
+    PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
+    # When the taq command is init
+    if [ "$INPUT_TASK" == "init" ]; then
+        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+        cd "$INPUT_PROJECT_NAME" || exit 1
+        npm init -y
+    else
+        echo "$PROJECT_DIR"
+        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+    fi
+fi
