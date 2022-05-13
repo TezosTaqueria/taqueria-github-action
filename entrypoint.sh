5af0ff5b903bc019b6f54d9420ea88732e2c6d4e
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 21:42:29 -0700

    chore: try manually set env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@
 #     /bin/bash
 # export PROJECT_DIR=/home/runner/work/taqueria-github-action/taqueria-github-action/test-project
 # export PROJECT_DIR=/home/gino/Documents/Repositories/taqueria-github-action/new-plugin-project
+export PROJECT_DIR=$RUNNER_TEMP
 if [ "$2" == "init" ]; then
     ls -la
     taq -p $1 $2
@@ -14,7 +15,10 @@ else
     echo $PROJECT_DIR
     echo $GITHUB_WORKSPACE
     echo $GITHUB_ENV
+    echo $GITHUB_REPOSITORY
     echo $RUNNER_WORKSPACE
+    echo $RUNNER_NAME
+    echo $RUNNER_TEMP
     cat /github/file_commands
     taq -p $1 $2
 fi
