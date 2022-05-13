93fc5d3c5ceba71892ef2d785e65badc5e61426d
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 21:11:02 -0700

    chore: check to see if GITHUB_WORKSPACE works

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,27 +3,15 @@
 # if $2 is equal to "compile", then run the following
 # if [ "$2" = "bash" ]; then
 #     /bin/bash
-export PROJECT_DIR=/home/runner/work/taqueria-github-action/taqueria-github-action/test-project
+# export PROJECT_DIR=/home/runner/work/taqueria-github-action/taqueria-github-action/test-project
 # export PROJECT_DIR=/home/gino/Documents/Repositories/taqueria-github-action/new-plugin-project
 if [ "$2" == "init" ]; then
     ls -la
     taq -p $1 $2
     cd $1
     npm init -y
-# elif [ "$2" == "compile" ]; then
-#     ls -la
-#     echo "The value of -p is $1"
-#     echo $PROJECT_DIR
-#     echo "Try the old way"
-#     cd $1
-#     taq compile
-#     echo "Try the new way"
-#     cd ..
-#     taq -p $1 compile
-#     ls -ltr ./test-project/artifacts
 else
-    ls -la
-    echo "The value of -p is $1"
     echo $PROJECT_DIR
+    echo $GITHUB_WORKSPACE
     taq -p $1 $2
 fi
