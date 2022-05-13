12d4fb036b8a5e9e2a9cc72cc24c390616252738
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 22:22:10 -0700

    chore: replace INPUT_TASK with

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$INPUT_TASK"
+echo "$2"
 if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$TASK_NAME" ];then
     echo "No project name or task name provided"
     exit 1
@@ -8,21 +8,21 @@ fi
 if [ -z "$INPUT_PROJECT_NAME" ];then
     PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
     if [ "$TASK_NAME" == "init" ]; then
-        taq "$INPUT_TASK"
+        taq "$2"
         npm init -y
     else
         echo "$PROJECT_DIR"
-        taq "$INPUT_TASK"
+        taq "$2"
     fi
 else
     PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
     # When the taq command is init
     if [ "$TASK_NAME" == "init" ]; then
-        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+        taq -p "$INPUT_PROJECT_NAME" "$2"
         cd "$INPUT_PROJECT_NAME" || exit 1
         npm init -y
     else
         echo "$PROJECT_DIR"
-        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+        taq -p "$INPUT_PROJECT_NAME" "$2"
     fi
 fi
