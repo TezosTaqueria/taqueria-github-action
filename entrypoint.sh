3474ba0da498f149e69484b62e10671a4ad17c16
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 22:33:47 -0700

    chore: debug issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,24 +5,15 @@ if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$TASK_NAME" ];then
     exit 1
 fi
 
-if [ -z "$INPUT_PROJECT_NAME" ];then
-    PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
-    if [ "$TASK_NAME" == "init" ]; then
-        taq "$2"
-        npm init -y
-    else
-        echo "$PROJECT_DIR"
-        taq "$2"
-    fi
+
+PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
+# When the taq command is init
+if [ "$TASK_NAME" == "init" ]; then
+    taq -p "$INPUT_PROJECT_NAME" "$2"
+    cd "$INPUT_PROJECT_NAME" || exit 1
+    ls -la
+    npm init -y
 else
-    PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
-    # When the taq command is init
-    if [ "$TASK_NAME" == "init" ]; then
-        taq -p "$INPUT_PROJECT_NAME" "$2"
-        cd "$INPUT_PROJECT_NAME" || exit 1
-        npm init -y
-    else
-        echo "$PROJECT_DIR"
-        taq -p "$INPUT_PROJECT_NAME" "$2"
-    fi
+    echo "$PROJECT_DIR"
+    taq -p "$INPUT_PROJECT_NAME" "$2"
 fi
