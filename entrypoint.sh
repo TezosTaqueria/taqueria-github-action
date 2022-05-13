1daa00e1e1ba1ebd269d24321b43fa62fd01aac4
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 22:15:14 -0700

    chore: added quotes to vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$TASK_NAME" ];then
+    echo "No project name or task name provided"
+    exit 1
+fi
 
 if [ -z "$INPUT_PROJECT_NAME" ];then
     PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
     if [ "$TASK_NAME" == "init" ]; then
-        taq $INPUT_TASK
+        taq "$INPUT_TASK"
         npm init -y
     else
-        taq $INPUT_TASK
+        echo "$PROJECT_DIR"
+        taq "$INPUT_TASK"
     fi
 else
     PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
     # When the taq command is init
     if [ "$TASK_NAME" == "init" ]; then
-        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
-        cd $INPUT_PROJECT_NAME
+        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+        cd "$INPUT_PROJECT_NAME" || exit 1
         npm init -y
     else
-        echo $PROJECT_DIR
-        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
+        echo "$PROJECT_DIR"
+        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
     fi
 fi
