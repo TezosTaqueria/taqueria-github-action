36f43cb6becaf0710aade5724f9afd44f1ea1edd
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-12 22:09:31 -0700

    chore: rework entrypoint.sh to handle an empty project_name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# If PROJECT_DIR is empty 
-if [ -z "$PROJECT_DIR" ];then
-    export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
-fi
-
-# When the taq command is init
-if [ "$2" == "init" ]; then
-    ls -la
-    taq -p $INPUT_PROJECT_NAME $INPUT_TASK
-    cd $1
-    npm init -y
+if [ -z "$INPUT_PROJECT_NAME" ];then
+    PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
+    if [ "$TASK_NAME" == "init" ]; then
+        taq $INPUT_TASK
+        npm init -y
+    else
+        taq $INPUT_TASK
+    fi
 else
-    echo $PROJECT_DIR
-    taq -p $INPUT_PROJECT_NAME $INPUT_TASK
+    PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
+    # When the taq command is init
+    if [ "$TASK_NAME" == "init" ]; then
+        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
+        cd $INPUT_PROJECT_NAME
+        npm init -y
+    else
+        echo $PROJECT_DIR
+        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
+    fi
 fi
