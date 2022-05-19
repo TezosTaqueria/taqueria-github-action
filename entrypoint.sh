2797328b5652e30f112d537f41b9799ebdc5dc4e
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-19 11:57:18 -0700

    chore: test new ligo-plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-echo "$INPUT_TASK"
-if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$TASK_NAME" ];then
-    echo "No project name or task name provided"
-    exit 1
+
+if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
+    echo "No project name, task name provided, or config file provided"
 fi
 
-if [ -z "$INPUT_PROJECT_NAME" ];then
+if [ -z "$INPUT_PROJECT_NAME" ]; then
     PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
-    if [ "$TASK_NAME" == "init" ]; then
-        taq "$INPUT_TASK"
+    if [ "$INPUT_TASK" == "init" ]; then
+        taq $INPUT_TASK
         npm init -y
     else
-        echo "$PROJECT_DIR"
-        taq "$INPUT_TASK"
+        taq $INPUT_TASK
     fi
 else
     PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
     # When the taq command is init
-    if [ "$TASK_NAME" == "init" ]; then
-        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+    if [ "$INPUT_TASK" == "init" ]; then
+        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
         cd "$INPUT_PROJECT_NAME" || exit 1
         npm init -y
     else
-        echo "$PROJECT_DIR"
-        taq -p "$INPUT_PROJECT_NAME" "$INPUT_TASK"
+        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
     fi
 fi
