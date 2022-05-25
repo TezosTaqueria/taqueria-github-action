dcd62f2296d440d7329a007a726c3a06386f6360
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-25 13:56:13 -0700

    chore: changed project name input to project directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ echo $GITHUB_WORKSPACE
 echo $RUNNER_WORKSPACE
 ls -ltr
 # Uncomment for local development
-if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
+if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
     # TODO: These are for testing and should be removed after
-    INPUT_PROJECT_NAME=$1
+    INPUT_PROJECT_DIRECTORY=$1
     INPUT_TASK=$2
     if [ "$INPUT_TASK" == "bash" ]; then
         /bin/bash
@@ -13,12 +13,12 @@ if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
 fi
 
 # echo "$INPUT_TASK"
-# if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
+# if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
 #     echo "No project name or task name provided"
 #     exit 1
 # fi
 
-if [ -z "$INPUT_PROJECT_NAME" ]; then
+if [ -z "$INPUT_PROJECT_DIRECTORY" ]; then
     export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
     # export NETWORK_OPTION="--network host"
     if [ "$INPUT_TASK" == "init" ]; then
@@ -28,17 +28,17 @@ if [ -z "$INPUT_PROJECT_NAME" ]; then
         taq $INPUT_TASK
     fi
 else
-    export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
+    export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_DIRECTORY
     # export NETWORK_OPTION="--network host"
     # When the taq command is init
     if [ "$INPUT_TASK" == "init" ]; then
-        taq -p $INPUT_PROJECT_NAME init
-        cd "$INPUT_PROJECT_NAME" || exit 1
+        taq -p $INPUT_PROJECT_DIRECTORY init
+        cd "$INPUT_PROJECT_DIRECTORY" || exit 1
         npm init -y
     # elif [[ "$INPUT_TASK" == *"start sandbox"* ]]; then
-    #     cd "$INPUT_PROJECT_NAME" || exit 1
+    #     cd "$INPUT_PROJECT_DIRECTORY" || exit 1
     #     taq start sandbox local
     else
-        taq -p $INPUT_PROJECT_NAME $INPUT_TASK
+        taq -p $INPUT_PROJECT_DIRECTORY $INPUT_TASK
     fi
 fi
