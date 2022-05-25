9827025ce2f55dce080a4329212c220d5fea2225
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-24 17:27:04 -0700

    chore: try new version and NETWORK_OPTION env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 # Uncomment for local development
-# if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
-#     # TODO: These are for testing and should be removed after
-#     INPUT_PROJECT_NAME=$1
-#     INPUT_TASK=$2
-#     if [ "$INPUT_TASK" == "bash" ]; then
-#         /bin/bash
-#     fi
-# fi
-
-echo "$INPUT_TASK"
 if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
-    echo "No project name or task name provided"
-    exit 1
+    # TODO: These are for testing and should be removed after
+    INPUT_PROJECT_NAME=$1
+    INPUT_TASK=$2
+    if [ "$INPUT_TASK" == "bash" ]; then
+        /bin/bash
+    fi
 fi
+
+# echo "$INPUT_TASK"
+# if [ -z "$INPUT_PROJECT_NAME" ] && [ -z "$INPUT_TASK" ]; then
+#     echo "No project name or task name provided"
+#     exit 1
+# fi
 
 if [ -z "$INPUT_PROJECT_NAME" ]; then
     export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
+    export NETWORK_OPTION=$NETWORK_OPTION
     if [ "$INPUT_TASK" == "init" ]; then
         taq init
         npm init -y
@@ -26,11 +27,15 @@ if [ -z "$INPUT_PROJECT_NAME" ]; then
     fi
 else
     export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_NAME
+    export NETWORK_OPTION=$NETWORK_OPTION
     # When the taq command is init
     if [ "$INPUT_TASK" == "init" ]; then
         taq -p $INPUT_PROJECT_NAME init
         cd "$INPUT_PROJECT_NAME" || exit 1
         npm init -y
+    # elif [[ "$INPUT_TASK" == *"start sandbox"* ]]; then
+    #     cd "$INPUT_PROJECT_NAME" || exit 1
+    #     taq start sandbox local
     else
         taq -p $INPUT_PROJECT_NAME $INPUT_TASK
     fi
