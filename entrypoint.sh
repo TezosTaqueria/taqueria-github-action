0c7aad2bc44017beacaf0414afd6435da10df625
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-25 17:14:11 -0700

    chore: added fresh project files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-echo $GITHUB_WORKSPACE
-echo $RUNNER_WORKSPACE
-ls -ltr
 # Uncomment for local development
-if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
-    # TODO: These are for testing and should be removed after
-    INPUT_PROJECT_DIRECTORY=$1
-    INPUT_TASK=$2
-    if [ "$INPUT_TASK" == "bash" ]; then
-        /bin/bash
-    fi
-fi
+# if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
+#     # TODO: These are for testing and should be removed after
+#     INPUT_PROJECT_DIRECTORY=$1
+#     INPUT_TASK=$2
+#     if [ "$INPUT_TASK" == "bash" ]; then
+#         /bin/bash
+#     fi
+# fi
 
 # echo "$INPUT_TASK"
-# if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
-#     echo "No project name or task name provided"
-#     exit 1
-# fi
+if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
+    echo "No project name or task name provided"
+    exit 1
+fi
 
 if [ -z "$INPUT_PROJECT_DIRECTORY" ]; then
     export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
@@ -31,22 +28,11 @@ else
     export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_DIRECTORY
     # export PROJECT_DIR=/home/gino/Documents/Repositories/taqueria-github-action/$INPUT_PROJECT_DIRECTORY
     echo $PROJECT_DIR
-    # When the taq command is init
     if [ "$INPUT_TASK" == "init" ]; then
         taq -p $INPUT_PROJECT_DIRECTORY init
-        echo "command exit code is $?"
         cd "$INPUT_PROJECT_DIRECTORY" || exit 1
         npm init -y
-    # elif [[ "$INPUT_TASK" == "bootstrap" ]]; then
-    #     taq -p $INPUT_PROJECT_DIRECTORY init
-    #     cd "$INPUT_PROJECT_DIRECTORY" || exit 1
-    #     npm init -y
-    #     taq install @taqueria/plugin-ligo@0.0.0-pr-741-86f0b45e
-    #     taq install @taqueria/plugin-flextesa@0.0.0-pr-741-86f0b45e
-    #     taq install @taqueria/plugin-taquito@0.0.0-pr-741-86f0b45e
-    #     taq start sandbox local
     else
         taq -p $INPUT_PROJECT_DIRECTORY $INPUT_TASK
-        echo "command exit code is $?"
     fi
 fi
--- example-projects/initialized-project/README.md
+++
@@ -1,5 +0,0 @@
-# Initialized Project
-
-## Overview
-
-This project has already been initialized and some plugins have already been installed. In this project we will start a Flextesa sandbox, compile a `.jsligo` contract, and deploy it to the local sandbox.
