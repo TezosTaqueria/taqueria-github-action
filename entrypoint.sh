ea11bd4dfc872eff34773951b38e6c3ddc61b8fc
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-05-26 13:03:32 -0700

    chore: prevent multiple npm init -y or taq init commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-# Uncomment for local development
-# if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
-#     # TODO: These are for testing and should be removed after
-#     INPUT_PROJECT_DIRECTORY=$1
-#     INPUT_TASK=$2
-#     if [ "$INPUT_TASK" == "bash" ]; then
-#         /bin/bash
-#     fi
-# fi
 
-# echo "$INPUT_TASK"
 WORKDIR=$(pwd)
 
 if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
@@ -18,21 +8,40 @@ if [ -z "$INPUT_PROJECT_DIRECTORY" ] && [ -z "$INPUT_TASK" ]; then
 fi
 
 if [ -z "$INPUT_PROJECT_DIRECTORY" ]; then
+
+    # Set the PROJECT_DIR variable if it has not already been set
     if [ -z "$PROJECT_DIR" ]; then
         export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}
+        echo "PROJECT_DIR has been set to $PROJECT_DIR"
     fi
-    echo $PROJECT_DIR
+
     taq init
-    npm init -y &> '/dev/null'
-    taq $INPUT_TASK
+
+    # The project will be initialized each time there is a task unless the task itself is to initialize the project
+    if [ "$INPUT_TASK" == "init" ]; then
+        npm init -y &> '/dev/null'
+    else
+        taq $INPUT_TASK
+    fi
+
 else
+
+    # Set the PROJECT_DIR variable if it has not already been set
     if [ -z "$PROJECT_DIR" ]; then
         export PROJECT_DIR=$RUNNER_WORKSPACE/${GITHUB_REPOSITORY#*/}/$INPUT_PROJECT_DIRECTORY
+        echo "PROJECT_DIR has been set to $PROJECT_DIR"
     fi
-    echo $PROJECT_DIR
+
     taq -p $INPUT_PROJECT_DIRECTORY init
-    cd "$INPUT_PROJECT_DIRECTORY" || exit 1
-    npm init -y &> '/dev/null'
-    cd $WORKDIR || exit 1
-    taq -p $INPUT_PROJECT_DIRECTORY $INPUT_TASK
+
+    # The project will be initialized each time there is a task unless the task itself is to initialize the project
+    if [ "$INPUT_TASK" == "init" ]; then
+        cd "$INPUT_PROJECT_DIRECTORY" || exit 1
+        npm init -y &> '/dev/null'
+        cd $WORKDIR || exit 1
+    else
+        taq -p $INPUT_PROJECT_DIRECTORY $INPUT_TASK
+    fi
+    
 fi
+    
