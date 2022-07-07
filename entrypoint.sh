35259b466ed8aa1e2d64a2cb0f0aafc9f2828682
GImbrailo <ginoimbrailo@gmail.com>
2022-07-07 13:28:23 -0700

    chore: try jq change to rpcUrl

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,9 @@ fi
 
 if [ -n "$INPUT_SANDBOX_NAME" ]; then
     taq start sandbox $INPUT_SANDBOX_NAME
+    echo "Setting $INPUT_SANDBOX_NAME sandbox rpc url to the correct host"
+    cat <<< "$(jq '.sandbox."$INPUT_SANDBOX_NAME".rpcUrl="http://172.17.0.1:20000"' .taq/config.json)" > .taq/config.json
+    jq '.sandbox."$INPUT_SANDBOX_NAME".rpcUrl'
 fi
 
 if [ "$INPUT_TESTS" == "true" ] || [ "$INPUT_TESTS" == "True" ]; then
