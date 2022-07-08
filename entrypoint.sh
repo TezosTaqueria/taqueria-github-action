40f3a41790311137a1abb47f30afada1d59ea8e6
GImbrailo <ginoimbrailo@gmail.com>
2022-07-07 17:07:02 -0700

    chore: remove debugging steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,9 +30,8 @@ fi
 
 if [ -n "$INPUT_SANDBOX_NAME" ]; then
     taq start sandbox $INPUT_SANDBOX_NAME
-    echo "Setting $INPUT_SANDBOX_NAME sandbox rpc url to the correct host"
+    echo "Setting $INPUT_SANDBOX_NAME sandbox to the correct IP for CICD runs"
     cat <<< "$(jq '.sandbox.'$INPUT_SANDBOX_NAME'.rpcUrl="http://172.17.0.1:20000"' .taq/config.json)" > .taq/config.json
-    jq '.sandbox."$INPUT_SANDBOX_NAME".rpcUrl' .taq/config.json
 fi
 
 if [ "$INPUT_TESTS" == "true" ] || [ "$INPUT_TESTS" == "True" ]; then
