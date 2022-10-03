0208ddbf8e5e1d11a030a7434d4fdb32bdb4f53a
GImbrailo <ginoimbrailo@gmail.com>
2022-10-03 14:18:23 -0700

    chore(entrypoint): refactored tests input

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,9 +59,10 @@ if [ -n "$INPUT_TASK" ] && [ "$INPUT_TASK" != "init" ]; then
     taq $INPUT_TASK
 fi
 
-if [ "$INPUT_TESTS" == "true" ] || [ "$INPUT_TESTS" == "True" ]; then
+if [ -n "$INPUT_TEST_PLUGIN" ]; then
     chmod -R 777 ./.taq
-    taq test
+    echo "Running tests using plugin $INPUT_TEST_PLUGIN"
+    taq test --plugin $INPUT_TEST_PLUGIN
     exit_code=$?
     chmod -R 755 ./.taq
     exit $exit_code
