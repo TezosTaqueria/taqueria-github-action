d74f680bdd3d124bfed89917460af3196a414bbe
GImbrailo <ginoimbrailo@gmail.com>
2023-01-30 12:00:40 -0800

    chore: try both smartpy and ligo tests again

--- example-projects/taco-shop/tests/taqueria.spec.ts
+++ example-projects/taco-shop/tests/taqueria.spec.ts
@@ -20,6 +20,11 @@ describe('E2E Testing for taqueria action', () => {
 		expect(accounts.stdout).toContain('artifacts/hello-tacos.tz');
 	});
 
+    test('Verify that taqueria can compile a previously registered contract with smartpy', async () => {
+		const accounts = await execPromise(`taq compile hello-tacos.py --plugin smartpy`, { cwd: `./` });
+		expect(accounts.stdout).toContain('artifacts/hello-tacos.tz');
+	});
+
     test('Verify that taqueria can originate a contract to the local sandbox', async () => {
         const contractName = 'hello-tacos.tz'
 
